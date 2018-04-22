9496b55582032602edae6a1314108dce79e58c45
SL0TR <mohaiminislam94@gmail.com>
2018-04-23 01:09:27 +0600

    change header UI of homepage

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,6 +1,26 @@
 <template>
   <q-page class="homepage">
     <div class="row">
+      <div class="col-12">
+        <q-carousel
+          arrows
+          height="50vh"
+          infinite
+          autoplay
+          quick-nav
+          class="round-borders shadow-1"
+        >
+          <q-carousel-slide img-src="../assets/company_cover_1.jpg">
+            <div class="absolute-bottom custom-caption"></div>
+          </q-carousel-slide>
+          <q-carousel-slide img-src="../assets/heading2.jpg">
+            <div class="absolute-bottom custom-caption"></div>
+          </q-carousel-slide>
+          <q-carousel-slide img-src="../assets/heading3.jpg">
+            <div class="absolute-bottom custom-caption"></div>
+          </q-carousel-slide>
+        </q-carousel>
+      </div>
       <div class="col-xs-8 offset-xs-2 col-lg-4 offset-lg-4 q-mt-lg">
         <div class="row">
           <div class="col-12">
@@ -9,13 +29,16 @@
             >
             </div>
           </div>
+          <div class="col-8 offset-2 q-mb-md">
+            <hr>
+          </div>
+          <div class="col-12  q-mt-md">
+            <div class="text-center q-headline text-primary">Ensuring quality one stitch at a time</div>
+          </div>
+          <div class="col-4 offset-4 q-mt-md">
+            <hr>
+          </div>
         </div>
-      </div>
-      <div class="col-12  q-mt-md">
-        <div class="text-center q-headline text-primary">Ensuring quality one stitch at a time</div>
-      </div>
-      <div class="col-2 offset-5 q-mb-md">
-        <hr>
       </div>
     </div>
   </q-page>
@@ -23,16 +46,26 @@
 
 <style scoped>
   .profilePic {
-    min-height: 250px;
+    min-height: 150px;
   }
 
   .back-image{
     background-size: 100% 100%;
   }
 
+  .q-carousel-slide {
+    background-size: 100% 100% !important;
+  }
+
   @media (max-width: 700px) {
     .profilePic {
       min-height: 150px;
+    }
+  }
+
+  @media (min-width: 1700px) {
+    .profilePic {
+      min-height: 200px;
     }
   }
 
