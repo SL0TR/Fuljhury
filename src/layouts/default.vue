9e1a1b5641fc4f1dce289678b7479c2e9f4100be
SL0TR <mohaiminislam94@gmail.com>
2018-04-22 13:56:33 +0600

    make navigation in toolbar phase 1

--- src/layouts/default.vue
+++ src/layouts/default.vue
@@ -2,11 +2,27 @@
   <q-layout view="lHh Lpr lFf">
     <q-layout-header>
       <q-toolbar
-        color="primary"
+        color="white"
       >
         <q-toolbar-title>
-          Fuljhuri
+          <img class="fuljhury-logo" src="../assets/fuljhury-logo.png" alt="fuljhury logo">
         </q-toolbar-title>
+      <q-btn-group class="text-light-blue">
+        <q-btn label="HOME" outline/>
+        <q-btn-dropdown class="text-light-blue" label="AbOUT US" outline>
+          <q-list link>
+            <q-item>
+              <q-item-main>
+                <q-item-tile label>COMPANY</q-item-tile>
+                <q-item-tile label>FACTORY</q-item-tile>
+                <q-item-tile label>CERTIFICATIONS</q-item-tile>
+              </q-item-main>
+            </q-item>
+          </q-list>
+        </q-btn-dropdown>
+        <q-btn label="PRODUCT" outline/>
+        <q-btn label="CONTACT" outline/>
+      </q-btn-group>
       </q-toolbar>
     </q-layout-header>
     <q-page-container>
@@ -26,5 +42,9 @@ export default {
 }
 </script>
 
-<style>
+<style scoped>
+  .fuljhury-logo {
+    height: 3rem;
+  }
+
 </style>
