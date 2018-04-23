6f06b478ec811372d7abd9ddab560ab6ca7b7567
SL0TR <mohaiminislam94@gmail.com>
2018-04-23 15:17:09 +0600

    fix minor nav bugs

--- src/components/navBar.vue
+++ src/components/navBar.vue
@@ -7,24 +7,22 @@
     </q-toolbar-title>
   <q-btn-group class="text-primary nav-btns" v-if="this.$q.platform.is.desktop">
     <q-btn label="HOME" outline @click="$router.push('/')"/>
-    <q-btn-dropdown class="text-primary" label="AbOUT US" outline split>
-      <q-list link class="text-primary">
-        <q-item>
-          <q-item-main>
-            <q-item-tile label @click="$router.push('/company')">COMPANY</q-item-tile>
-          </q-item-main>
-        </q-item>
-        <q-item>
-          <q-item-main>
-            <q-item-tile label @click="$router.push('/factory')">FACTORY</q-item-tile>
-          </q-item-main>
-        </q-item>
-        <q-item>
-          <q-item-main>
-            <q-item-tile label @click="$router.push('/certifications')">CERTIFICATIONS</q-item-tile>
-          </q-item-main>
-        </q-item>
-      </q-list>
+    <q-btn-dropdown class="text-primary" label="ABOUT US" outline>
+      <div class="row">
+        <div class="col-12 text-center">
+          <q-btn class="text-primary" label="COMPANY" flat @click="$router.push('/company')"/>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-12 text-center">
+          <q-btn class="text-primary" label="FACTORY" flat @click="$router.push('/factory')"/>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-12 text-center">
+         <q-btn class="text-primary" label="CERTIFICATIONS" flat @click="$router.push('/certifications')"/>
+        </div>
+      </div>
     </q-btn-dropdown>
     <q-btn label="PRODUCT" outline @click="$router.push('/product')"/>
     <q-btn label="CONTACT" outline @click="$router.push('/contact')"/>
@@ -35,25 +33,23 @@
         <q-btn class="full-width" label="HOME" outline @click="$router.push('/')"/>
       </div>
       <div class="col-10 offset-1 q-mt-lg">
-      <q-btn-dropdown class="text-primary full-width" label="AbOUT US" outline>
-        <q-list link class="text-primary">
-          <q-item>
-            <q-item-main>
-              <q-item-tile label @click="$router.push('/company')">COMPANY</q-item-tile>
-            </q-item-main>
-          </q-item>
-          <q-item>
-            <q-item-main>
-              <q-item-tile label @click="$router.push('/factory')">FACTORY</q-item-tile>
-            </q-item-main>
-          </q-item>
-          <q-item>
-            <q-item-main>
-              <q-item-tile label @click="$router.push('/certifications')">CERTIFICATIONS</q-item-tile>
-            </q-item-main>
-          </q-item>
-        </q-list>
-      </q-btn-dropdown>
+        <q-btn-dropdown class="text-primary full-width" label="ABOUT US" outline>
+          <div class="row">
+            <div class="col-12 text-center">
+              <q-btn class="text-primary" label="COMPANY" flat @click="$router.push('/company')"/>
+            </div>
+          </div>
+          <div class="row">
+            <div class="col-12 text-center">
+              <q-btn class="text-primary" label="FACTORY" flat @click="$router.push('/factory')"/>
+            </div>
+          </div>
+          <div class="row">
+            <div class="col-12 text-center">
+            <q-btn class="text-primary" label="CERTIFICATIONS" flat @click="$router.push('/certifications')"/>
+            </div>
+          </div>
+        </q-btn-dropdown>
       </div>
       <div class="col-10 offset-1 q-mt-lg">
         <q-btn class="full-width" label="PRODUCT" outline @click="$router.push('/product')"/>
@@ -85,6 +81,10 @@ export default {
 <style scoped>
   .fuljhury-logo {
     height: 3rem;
+  }
+
+  .q-item {
+    text-align: center;
   }
 
   @media (max-width: 700px) {
