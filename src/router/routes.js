87e0657cc57ada901d61746feef7963ea20b08cc
SL0TR <mohaiminislam94@gmail.com>
2018-05-19 20:21:16 +0600

    proper route property added

--- src/router/routes.js
+++ src/router/routes.js
@@ -4,12 +4,12 @@ export default [
     path: '/',
     component: () => import('layouts/default'),
     children: [
-      { path: '', component: () => import('pages/index') },
-      { path: 'company', component: () => import('pages/company') },
-      { path: 'factory', component: () => import('pages/factory') },
-      { path: 'certifications', component: () => import('pages/certifications') },
-      { path: 'product', component: () => import('pages/product') },
-      { path: 'contact', component: () => import('pages/contact') }
+      { path: '', name: 'fuljhury', component: () => import('pages/index'), meta: { title: 'Fuljhury' } },
+      { path: 'company', name: 'company', component: () => import('pages/company'), meta: { title: 'Company' } },
+      { path: 'factory', name: 'factory', component: () => import('pages/factory'), meta: { title: 'Factory' } },
+      { path: 'certifications', name: 'certficiations', component: () => import('pages/certifications'), meta: { title: 'Certifications' } },
+      { path: 'product', name: 'product', component: () => import('pages/product'), meta: { title: 'Product' } },
+      { path: 'contact', name: 'contact', component: () => import('pages/contact'), meta: { title: 'Contact' } }
     ]
   },
 
